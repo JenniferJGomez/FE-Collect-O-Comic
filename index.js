3b88b5769f51d082a82cbbee69527fd9cdf8f543
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-02-07 17:46:30 -0500

    added comic cards section

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 document.addEventListener("DOMContentLoaded", ()=> {
     getNerdCollection()
     getAllComics()
+    getSearchform().submit.addEventListener('click', submittingForm)
+    
 })
+
+function getSearchform(){
+    return document.querySelector('#search-bar')
+}
+function submittingForm(e){
+    e.preventDefault()
+    let parentForm = e.target.parentElement
+    let userInput =  parentForm.userInput.value
+    console.log('submiting')
+}
+
+
 
 function getNerdCollection(){
     let userCollectionBtn = document.querySelector('#user-collection')

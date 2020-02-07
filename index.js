beef2f4d2a2f2d341900022a96800dd039e7c5b7
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-02-07 16:50:44 -0500

    amia working on html css and some js. event listener added on button

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-   
+    getNerdCollection()
+    getAllComics()
 })
 
+function getNerdCollection(){
+    let userCollectionBtn = document.querySelector('#user-collection')
+    userCollectionBtn.addEventListener('click',()=>console.log("getting user collection"))
+    
+}
+
+function getAllComics(){
+    let allComicBtn = document.querySelector('#all-comic')
+    allComicBtn.addEventListener('click', () => console.log('getting all comics'))
+}
